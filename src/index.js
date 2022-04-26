e29a16dea1ab904117a6965d468f0aac075c76ba
Yonata Jaraba <ygjr_123@hotmail.com>
2022-04-25 21:45:14 -0500

    cambiando tema

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootswatch/dist/quartz/bootstrap.min.css';
-
+  
 import './firebase'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
