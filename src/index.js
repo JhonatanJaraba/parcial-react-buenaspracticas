1471dbb46cb1b205b619cfad52a3a591c1f5112d
Yonata Jaraba <ygjr_123@hotmail.com>
2022-04-25 21:44:17 -0500

    eliminar usuarios

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import 'bootswatch/dist/superhero/bootstrap.min.css';
+import 'bootswatch/dist/quartz/bootstrap.min.css';
 
 import './firebase'
 const root = ReactDOM.createRoot(document.getElementById('root'));
